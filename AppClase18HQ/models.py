f89278137cc2f14d93aade2322905705d68fa5ac
Henry Quilca Rojas <henryquro@gmail.com>
2023-11-14 15:42:43 -0500

    Usando plantillas

--- AppClase18HQ/models.py
+++ AppClase18HQ/models.py
@@ -2,12 +2,32 @@ from django.db import models
 
 
 # Create your models here.
-class Curso(models.Model):  # Ponemos la herencia models.Model
-    nombre = models.CharField(max_length=40)  # Tendrá como máximo 40 caracteres (tipo str).
-    camada = models.IntegerField()  # De tipo int.
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"Curso: {self.nombre}, Camada: {self.camada}"
 
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    corro = models.EmailField()
+    email = models.EmailField()
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Profesor: {self.nombre} {self.apellido}"
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)
+    fecha_de_entrega = models.DateField()
+    entregado = models.BooleanField()
